c14ddeb87090b78ac62f51aab85c28bbfc0bae3b
Anton Iun <iun@edgeconsult.me>
2018-04-29 04:23:59 +0300

    Create ProcessVideo job

---
+++ app/Jobs/ProcessVideo.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Storage;
+
+class ProcessVideo implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $path;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // Do job here
+        Storage::disk('uploads')->delete($this->path);
+    }
+}
