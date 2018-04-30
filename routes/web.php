a2d8de4463aea180ced91a3a6620a448ebc5eb06
Anton Iun <iun@edgeconsult.me>
2018-04-30 10:18:44 +0300

    Set restricts on filename

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/video', 'VideoController');
 
-Route::get('/videos/{video}/{file}', 'VideoController@getAsset');
+Route::get('/videos/{video}/{file}', 'VideoController@getAsset')
+    ->where('file', '(.*)');
