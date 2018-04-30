97b7000681923f0303f3390a2a35236e67194ff3
Anton Iun <iun@edgeconsult.me>
2018-04-30 07:48:53 +0300

    Increase timeout value of ProcessVideo job

--- app/Jobs/ProcessVideo.php
+++ app/Jobs/ProcessVideo.php
@@ -21,6 +21,13 @@ class ProcessVideo implements ShouldQueue
      */
     public $tries = 1;
 
+    /**
+     * The number of seconds the job can run before timing out.
+     *
+     * @var int
+     */
+    public $timeout = 14400;
+
     protected $path;
 
     /**
