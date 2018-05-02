cc2f547e250b78c56dbd57cc535284b45158e126
Anton Iun <iun@edgeconsult.me>
2018-05-03 00:02:25 +0300

    Create form with basic info

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,12 +8,9 @@
                 <img class="img-fluid" src="{{ url('/square.png') }}" alt="photo">
             </div>
             <div class="col-md-9">
-                <h3 class="card-title">정은주</h3>
-                <p class="card-text text-muted">Age: 28</p>
-                <p class="card-text text-muted">Height: 180</p>
-                <p class="card-text text-muted">Weigth: 70</p>
-                <p class="card-text text-muted">BMI: 20</p>
-                <div class="card-subtitle text-muted">
+                <h3 class="card-title">杜建军</h3>
+                <div class="card-text">
+                    <h5>Basic</h5>
                     <ul class="list-inline">
                         <li class="list-inline-item">Age: 28</li>
                         <li class="list-inline-item">Height: 180</li>
@@ -21,8 +18,11 @@
                         <li class="list-inline-item">BMI: 20</li>
                     </ul>
                 </div>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id sapien eu augue tincidunt auctor consequat ac diam. In sit amet quam nunc. Proin quis tellus dapibus, pulvinar mi et, interdum dolor. Maecenas finibus volutpat ex quis condimentum. Quisque interdum felis purus, id lobortis ligula interdum nec. Morbi et massa sollicitudin, commodo dolor eget, rhoncus leo. Quisque tempus sagittis fringilla.</p>
-                <a href="#" class="btn btn-primary">Leave message to him</a>
+                <div class="card-text">
+                    <h5>About himself</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id sapien eu augue tincidunt auctor consequat ac diam. In sit amet quam nunc. Proin quis tellus dapibus, pulvinar mi et, interdum dolor. Maecenas finibus volutpat ex quis condimentum. Quisque interdum felis purus, id lobortis ligula interdum nec. Morbi et massa sollicitudin, commodo dolor eget, rhoncus leo. Quisque tempus sagittis fringilla.</p>
+                </div>
+                <a href="#" class="btn btn-primary">Say "Hi!" to him</a>
             </div>
         </div>
     </div>
