bca8c3c23152971afedfd5cc3d74cef5bb23b8bb
Anton Iun <iun@edgeconsult.me>
2018-05-02 09:33:45 +0300

    Add title to video show page

--- resources/views/video/show.blade.php
+++ resources/views/video/show.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.player')
 
 @section('content')
-<div class="container text-center">
+<div class="container">
     @if (session('status'))
         <div class="alert alert-success">
             {{ session('status') }}
         </div>
     @endif
-    <video id="video" class="video-js vjs-16-9 vjs-big-play-centered"></video>
+    <video id="video" class="video-js vjs-16-9 vjs-big-play-centered mb-3"></video>
+    <div>
+        <h2>{{ $video->title }}</h2>
+    </div>
 </div>
 @endsection
